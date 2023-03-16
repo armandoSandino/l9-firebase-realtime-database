48847163bed301fb77a53f26af366e54c5964448
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-16 17:20:36 -0600

    feat; add route web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FirebaseController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('get-firebase-data', [FirebaseController::class, 'index'])
+->name('firebase.index');
 
 Route::get('/', function () {
     return view('welcome');
